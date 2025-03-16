44bd64a2fc22ad8eda74c3b0edf304fd253e4cab
celzadinata <dinatacelza@gmail.com>
2025-03-16 15:03:01 +0700

    initial commit

---
+++ src/main/java/com/enigmacamp/HikeMateApplication.java
@@ -0,0 +1,13 @@
+package com.enigmacamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikeMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HikeMateApplication.class, args);
+	}
+
+}
